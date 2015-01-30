3266aebda01477552b2c5c08e331779d3268fbed
Emilio3433 <eserrano3@mail.stmarystx.edu>
2015-01-29 21:15:40 -0700

    Challenge1

---
+++ workspace/Challenge1/src/Challenge1.java
@@ -0,0 +1,14 @@
+public class Challenge1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double annualPay = 10000.0;
+		double age = 26;
+		String name = " Joe Mahoney";
+
+		System.out.printf("My Name Is" + name + ", My Age Is " + age+ " I hope to earn $ " + annualPay + " per year ");
+
+	}
+
+}              
+	 
