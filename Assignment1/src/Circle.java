2da7de9a96281098d1370d03490bbba3c110c068
Emilio3433 <eserrano3@mail.stmarystx.edu>
2015-01-29 20:59:54 -0700

    Intitial check in of assignment 1.

---
+++ Assignment1/src/Circle.java
@@ -0,0 +1,12 @@
+
+public class Circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int r = 5;
+		
+		System.out.println("The circle's radius is " +r+ ", the area is " + (Math.PI * (r*r)) + "\nThe diameter is " + 2 * r); 
+	}
+
+}
